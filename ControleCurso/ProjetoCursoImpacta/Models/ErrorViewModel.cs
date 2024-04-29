a255f16b51bc8c72eb00941dc04c0ba0e3d82a76
Kaltiane Maria Gomes Da Silva Montenegro <kalfisio@hotmail.com>
2024-04-28 22:41:03 -0300

    add ControleCurso MVC

---
+++ ControleCurso/ProjetoCursoImpacta/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjetoCursoImpacta.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
